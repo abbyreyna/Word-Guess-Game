cbee7b94590c3b35508f7dd65636292dea25622f
Abby ❤️ <reyna.abby08@gmail.com>
2019-02-18 14:14:59 -0800

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,21 @@
+window.addEventListener("DOMContentLoaded", function(){
+
+
 $(document).ready(function() {
 
-    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var word; //word to guess
+    var winCounter; //# of times user guessed the word correctly
+    var guessCounter; //# of guesses remaining for the user
+    var space; //spaces in a word
+
+    //get elements
+    var showWins = getElementByID("wins"); //stored wins
+    var showGuessesRemaining = getElementByID("userGuesses");
+    var showLettersGuessed = getElementByID("lettersGuessed"); //letters the user has guessed
+
+    //alphabet
+    for (var i = 0; i < letters.length; i++)
+});
 
 });
